5285f3d4308f651ff91d7b85b7321924aea3e585
rualpa2004 <146848892+rualpa2004@users.noreply.github.com>
2026-02-17 11:51:28 +0100

    visual changes

--- backend/tickethub/src/main/java/es/tickethub/tickethub/entities/Discount.java
+++ backend/tickethub/src/main/java/es/tickethub/tickethub/entities/Discount.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,6 +16,10 @@ import lombok.Setter;
 public class Discount {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long discountID;
+
+    @Column(unique = true)
     private String discountName;
 
     @Column(nullable = false)
