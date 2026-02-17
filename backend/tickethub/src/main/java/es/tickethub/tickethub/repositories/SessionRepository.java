4fa93f1df8eaae45ff1625575efe7d0ca1e5a09f
shania <119068473+shaniaconx@users.noreply.github.com>
2026-02-17 22:58:48 +0100

    Discount done for now

--- backend/tickethub/src/main/java/es/tickethub/tickethub/repositories/SessionRepository.java
+++ backend/tickethub/src/main/java/es/tickethub/tickethub/repositories/SessionRepository.java
@@ -1,20 +1,5 @@
 package es.tickethub.tickethub.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import es.tickethub.tickethub.entities.Session;
-
-import java.sql.Timestamp;
-import java.util.List;
-
-
-
-public interface SessionRepository extends JpaRepository<Session, Long>{
-    // It searches an attribute Event in Session then enters Event and picks the EventID
-    List<Session> findByEvent_EventID(Long eventID);
-
-    // Search for a session starting now or on a specific day and time
-    List<Session> findByDateAfter(Timestamp date);
-
-    // Search for a session on a specific day regardless of the time
-    List<Session> findByDateBetween(Timestamp start, Timestamp end);
+public class SessionRepository {
+    
 }
--- backend/tickethub/src/main/java/es/tickethub/tickethub/repositories/TicketRepository.java
+++ backend/tickethub/src/main/java/es/tickethub/tickethub/repositories/TicketRepository.java
@@ -1,9 +1,5 @@
 package es.tickethub.tickethub.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.tickethub.tickethub.entities.Ticket;
-
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
+public class TicketRepository {
     
 }
