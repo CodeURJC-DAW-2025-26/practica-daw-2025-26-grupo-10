abcd9e51327663a9029c1767a3b8d8fc6fb84855
fuihfuefuiewn <danielbonachelamartinez@gmail.com>
2026-02-18 12:14:00 +0100

    Update ZoneService.java
    Zone is done, finally. Now I will move into artists.
    I have fixed a few bugs in Shania's part and create a .java suggested by Ruben

--- backend/tickethub/src/main/java/es/tickethub/tickethub/repositories/ZoneRepository.java
+++ backend/tickethub/src/main/java/es/tickethub/tickethub/repositories/ZoneRepository.java
@@ -3,9 +3,9 @@ package es.tickethub.tickethub.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import es.tickethub.tickethub.entities.Zone;
-/**Internamente spring crea una "clase mágica" con la que hace el CRUD.
- * Id es Zone y es de tipo Long
-*/
+
+//Id = Zone, type = Long
+
 
 public interface ZoneRepository extends JpaRepository<Zone, Long> {
 }
--- backend/tickethub/src/main/java/es/tickethub/tickethub/services/ZoneService.java
+++ backend/tickethub/src/main/java/es/tickethub/tickethub/services/ZoneService.java
@@ -1,9 +1,12 @@
 package es.tickethub.tickethub.services;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import es.tickethub.tickethub.entities.Zone;
 import es.tickethub.tickethub.repositories.ZoneRepository;
@@ -18,14 +21,30 @@ public class ZoneService {
     }
 
     public List<Zone> findAll() {
-        return zoneRepository.findAll();
+        List <Zone> zone = zoneRepository.findAll();
+        if (!(zone.isEmpty())){
+            return zone;
+        }
+        throw new ResponseStatusException(HttpStatus.NO_CONTENT, "No hay zonas registradas");
     }
 
-    public Optional<Zone> findById(Long id) {
-        return zoneRepository.findById(id);
+    public Zone findById(Long id) {
+        Optional <Zone> zoneOptional = zoneRepository.findById(id);
+        if (zoneOptional.isPresent()){
+            return zoneOptional.get();
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Zona no encontrada");
     }
 
     public Zone save(Zone zone) {
+        if (zone.getCapacity() <= 0) {
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "La capacidad debe ser mayor que 0");
+        }
+
+        if (zone.getPrice().compareTo(BigDecimal.ZERO) < 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El precio no puede ser negativo");
+        }
+
         return zoneRepository.save(zone);
     }
 
