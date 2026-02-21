7357f654bd8168b119dce9639be50bf4ee12b5f1
rualpa2004 <146848892+rualpa2004@users.noreply.github.com>
2026-02-21 13:03:30 +0100

    applying mustache to all html archives of the public folder and changing default event things

--- backend/tickethub/src/main/java/es/tickethub/tickethub/services/DataBaseInitializer.java
+++ backend/tickethub/src/main/java/es/tickethub/tickethub/services/DataBaseInitializer.java
@@ -24,14 +24,15 @@ import es.tickethub.tickethub.entities.Purchase;
 import es.tickethub.tickethub.entities.Session;
 import es.tickethub.tickethub.entities.Ticket;
 import es.tickethub.tickethub.entities.Zone;
+
 import es.tickethub.tickethub.repositories.ArtistRepository;
 import es.tickethub.tickethub.repositories.DiscountRepository;
 import es.tickethub.tickethub.repositories.EventRepository;
 import es.tickethub.tickethub.repositories.PurchaseRepository;
 import es.tickethub.tickethub.repositories.SessionRepository;
-import es.tickethub.tickethub.repositories.TicketRepository;
 import es.tickethub.tickethub.repositories.UserRepository;
 import es.tickethub.tickethub.repositories.ZoneRepository;
+
 import jakarta.annotation.PostConstruct;
 
 @Service
@@ -42,9 +43,6 @@ public class DataBaseInitializer {
 
     @Autowired
     private SessionRepository sessionRepository;
-
-    @Autowired
-    private TicketRepository ticketRepository;
 
     @Autowired
     private PurchaseRepository purchaseRepository;
@@ -105,11 +103,17 @@ public class DataBaseInitializer {
         );
 
         List <Image> clientImages = Arrays.asList(
-            new Image("clientImage1", convertToBlob(loadImage("database-diagram.png")))
+            new Image("clientImage1", convertToBlob(loadImage("default-avatar.png")))
+        );
+
+        List <Image> eventImages = Arrays.asList(
+            new Image("eventImage1", convertToBlob(loadImage("classes-diagram.png"))),
+            new Image("eventImage2", convertToBlob(loadImage("database-diagram.png")))
         );
 
         allImages.addFirst(artistImages);   //We add the artistImages list to the big list
         allImages.add(1, clientImages);
+        allImages.add(2, eventImages);
 
         return allImages;
     }
@@ -117,18 +121,18 @@ public class DataBaseInitializer {
     public List<Artist> initializeArtists(List <Image> artistImages) {
 
         List<Artist> artists = Arrays.asList(
-            new Artist("Duki", "", artistImages.get(0), "", ""),
-            new Artist("Film Symphony Orchestra", "", artistImages.get(1), "", ""),
-            new Artist("Aitana", "", artistImages.get(2), "", ""),
-            new Artist("Juan Dávila", "", artistImages.get(3), "", ""),
-            new Artist("Galder Varas", "", artistImages.get(4), "", ""),
-            new Artist("Carlos Vives", "", artistImages.get(5), "", ""),
-            new Artist("Vetusta Morla", "", artistImages.get(6), "", ""),
-            new Artist("David Guetta", "", artistImages.get(7), "", ""),
-            new Artist("Diana Krall", "", artistImages.get(8), "", ""),
-            new Artist("Alejandro Sanz", "", artistImages.get(9), "", ""),
-            new Artist("Niña Pastori", "", artistImages.get(10), "", ""),
-            new Artist("Metallica", "", artistImages.get(11), "", "")
+            new Artist("Duki", "Cantante argentino nacido en Buenos Aires el 24/06/1996. Sus temas abarcan sobre todo música urbana (trap, reggaeton)",artistImages.get(0), "duki", "DukiSSJ"),
+            new Artist("Film Symphony Orchestra", "",artistImages.get(1), "", ""),
+            new Artist("Aitana", "",artistImages.get(2), "", ""),
+            new Artist("Juan Dávila", "",artistImages.get(3), "", ""),
+            new Artist("Galder Varas", "",artistImages.get(4), "", ""),
+            new Artist("Carlos Vives", "",artistImages.get(5), "", ""),
+            new Artist("Vetusta Morla", "",artistImages.get(6), "", ""),
+            new Artist("David Guetta", "",artistImages.get(7), "", ""),
+            new Artist("Diana Krall", "",artistImages.get(8), "", ""),
+            new Artist("Alejandro Sanz", "",artistImages.get(9), "", ""),
+            new Artist("Niña Pastori", "",artistImages.get(10), "", ""),
+            new Artist("Metallica", "",artistImages.get(11), "", "")
         );
 
         artistRepository.saveAll(artists);
@@ -136,7 +140,7 @@ public class DataBaseInitializer {
         return artists;
     }
 
-    public List<Event>  initializeEvents(List<Artist> artists) {
+    public List<Event>  initializeEvents(List<Artist> artists, List<Image> eventImages) {
 
         /* This is for the initializeZones function. If u want to add zones to any event do it a set/list/array and pass it here
         To add anything (zones, sessions or discounts) u have to do events.get().getzones/getSessions/getDiscounts().add(zone/session/discount)
@@ -146,7 +150,7 @@ public class DataBaseInitializer {
         */
 
         List<Event> events = Arrays.asList(
-            new Event("Concierto Duki Wizink Center", 20000, artists.get(0), null, null, "Wizink Center", "Música", null),
+            new Event("Concierto Duki Wizink Center", 20000, artists.get(0), null, null, "Wizink Center", "Música", eventImages),
             new Event("Film Symphony Orchestra Wizink Center", 20000, artists.get(1), null, null, "Wizink Center", "Música", null),
             new Event("Concierto Aitana Wizink Center", 20000, artists.get(2), null, null, "Wizink Center", "Música", null),
             new Event("El show de Juan Dávila", 15000, artists.get(3), null, null, "Palacio Vistalegre", "Comedia", null),
@@ -161,14 +165,18 @@ public class DataBaseInitializer {
             new Event("Metal Legends Live", 20000, artists.get(11), null, null, "Auditorio Miguel Ríos", "Metal", null)
         );
 
+        artists.get(0).getLastEvents().add(events.get(0));
+        artists.get(0).getEventsIncoming().add(events.get(9));
+
         eventRepository.saveAll(events);
-        
+        artistRepository.saveAll(artists);
+
         return events; 
     }
 
     public Client initializeUsers(List <Image> clientImages) {
 
-        Client defaultClient = new Client("pepe@gmail.com", "PepeG", "pepe123", "Pepe", "Garcia", 33, 666666666, BigDecimal.ZERO, null, null, null);
+        Client defaultClient = new Client("pepe@gmail.com", "PepeG", "pepe123", "Pepe", "Garcia", 33, 666666666, BigDecimal.ZERO, null, null, clientImages.get(0));
         Admin defaultAdmin = new Admin("adminEmail@gmail.com", "newAdmin", "admin");
 
         userRepository.save(defaultClient);
@@ -204,27 +212,25 @@ public class DataBaseInitializer {
     public List<Session> initializeSessions(List<Event> events) {
 
         List<Session> sessions = Arrays.asList(
-            new Session(events.get(0), Timestamp.valueOf("2026-06-10 21:00:00")),
-            new Session(events.get(1), Timestamp.valueOf("2026-06-11 21:00:00")),
-            new Session(events.get(2), Timestamp.valueOf("2026-06-12 21:00:00"))
+            new Session(events.get(0), null, Timestamp.valueOf("2026-06-10 21:00:00")),
+            new Session(events.get(1), null, Timestamp.valueOf("2026-06-11 21:00:00")),
+            new Session(events.get(2), null, Timestamp.valueOf("2026-06-12 21:00:00"))
         );
 
         sessionRepository.saveAll(sessions);
         return sessions;
     }
 
-    public void initializePurchases(Client client, Session session) {
+    public void initializePurchases(Client client, Session session, List <Zone> zones) {
 
             List<Ticket> tickets = Arrays.asList(
-            new Ticket("QR1", session, new BigDecimal("45.00"), true),
-            new Ticket("QR2", session, new BigDecimal("45.00"), true),
-            new Ticket("QR3", session, new BigDecimal("80.00"), true),
-            new Ticket("QR4", session, new BigDecimal("120.00"), true)
+            new Ticket("QR1", zones.get(0), true),
+            new Ticket("QR2", zones.get(0), true),
+            new Ticket("QR3", zones.get(0), true),
+            new Ticket("QR4", zones.get(0), true)
         );
 
-    Purchase purchase = new Purchase(tickets, session, client);
-
-        
+        Purchase purchase = new Purchase(tickets, session, client);
 
         purchaseRepository.save(purchase);
     }
@@ -239,15 +245,15 @@ public class DataBaseInitializer {
 
         List <Artist> artists = initializeArtists(images.get(0));
 
-        List<Event> events = initializeEvents(artists);
+        List<Event> events = initializeEvents(artists, images.get(2));
 
-        List<Discount> discounts = initializeDiscounts();
+        initializeDiscounts();
 
         List<Zone> zones = initializeZones();
 
         List<Session> sessions = initializeSessions(events);
 
-        initializePurchases(defaultClient, sessions.get(0));
+        initializePurchases(defaultClient, sessions.get(0), zones);
     }
 
 }
