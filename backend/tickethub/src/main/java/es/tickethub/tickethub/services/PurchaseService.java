4ad14a14e2d1f47908d60f221d215db89feb892a
fuihfuefuiewn <danielbonachelamartinez@gmail.com>
2026-02-21 22:34:50 +0100

    3 Merge Conflict
    Co-Authored-By: SebasSilv <146847773+d4ng3r25@users.noreply.github.com>
    Co-Authored-By: rualpa2004 <146848892+rualpa2004@users.noreply.github.com>

--- backend/tickethub/src/main/java/es/tickethub/tickethub/services/PurchaseService.java
+++ backend/tickethub/src/main/java/es/tickethub/tickethub/services/PurchaseService.java
@@ -1,6 +1,8 @@
 package es.tickethub.tickethub.services;
 
 import java.util.List;
+
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
