7014bddcc97298ff8e1b20868a744b0a4ab226e5
rualpa2004 <146848892+rualpa2004@users.noreply.github.com>
2026-02-25 14:26:40 +0100

    All CRUD of sessions working and create and edit event also working too

--- backend/tickethub/src/main/resources/static/js/eventThings.js
+++ backend/tickethub/src/main/resources/static/js/eventThings.js
@@ -1,6 +1,5 @@
 /*LO QUE FALTA POR PONER EN ESTE JS:
-- De session: js para edit y enlazarlo con el edit_session del backend (Si puedes hacer el js que necesito para editar
-  perfecto sino ya lo haré yo con el add que hay y ayudándome de ChatGPT, yo voy a tocar el session controller para adecuar las rutas, ESO NO LO TOQUES)
+- De session: NADA, YA LO HE HECHO Y FUNCIONA
 - De discount: arreglar que se puedan crear líneas de descuentos si no hay ninguna (si eliminas todas el botón se bloquea)
 - De zone: hacer exactamente lo mismo que con discounts (que puedan añadirse filas haya o no haya ninguna)
 
@@ -42,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
-//For create new Sessions
+//For the create and edit functions of sessions
 document.addEventListener('DOMContentLoaded', () => {
 
   //Consts to get all the elements we need to work with sessions
@@ -90,19 +89,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
   });
 
-  //A PARTIR DE ESTO ESTÁ MAL, POR ESO ESTÁ COMENTADO
-
-  // Edit existing session
-  /*document.querySelectorAll('.edit-session').forEach(btn => {
+  // Edit existing session working
+  document.querySelectorAll('.edit-session').forEach(btn => {
 
     btn.addEventListener('click', function () {
 
       const row = this.closest('tr');
       const sessionId = row.dataset.id;
+      const eventId = row.dataset.eventid;
       const dateCell = row.querySelector('.date-cell');
       const actionsCell = row.querySelector('.actions-cell');
-
-      const currentText = dateCell.innerText;
 
       dateCell.innerHTML =
         `<input type="datetime-local" class="form-control edit-date">`;
@@ -116,10 +112,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const newDate = row.querySelector('.edit-date').value;
 
-        fetch('/admin/events/update_session/' + sessionId, {
-          method: 'PUT',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ date: newDate })
+        const data = new URLSearchParams();
+        data.append('newDate', newDate);
+        data.append('sessionID', sessionId);
+        if (!newDate) {
+          alert("Debes seleccionar una fecha");
+          return;
+        }
+
+        fetch('/admin/events/' + eventId + '/update_session', {
+          method: 'POST',
+          body: data
         })
         .then(res => {
           if (res.ok) location.reload();
@@ -129,6 +132,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     });
 
-  });*/
+  });
 
 });
