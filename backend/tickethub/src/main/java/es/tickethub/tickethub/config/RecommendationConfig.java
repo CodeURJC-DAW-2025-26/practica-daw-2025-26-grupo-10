8676d808409c0e88ed3409e5326690161cbac26b
fuihfuefuiewn <danielbonachelamartinez@gmail.com>
2026-02-28 19:29:21 +0100

    Recommendation adjustment

--- backend/tickethub/src/main/java/es/tickethub/tickethub/config/RecommendationConfig.java
+++ backend/tickethub/src/main/java/es/tickethub/tickethub/config/RecommendationConfig.java
@@ -1,33 +1,7 @@
 package es.tickethub.tickethub.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.server.ResponseStatusException;
-
-import es.tickethub.tickethub.entities.Event;
-import es.tickethub.tickethub.services.EventService;
-import es.tickethub.tickethub.services.ServerRecommendationService;
 
 @Configuration
 public class RecommendationConfig {
-
-    private final EventService eventService;
-
-    public RecommendationConfig(EventService eventService) {
-        this.eventService = eventService;
-    }
-
-    @Bean
-    public ServerRecommendationService serverRecommendationService() {
-        List<Event> events;
-        try {
-            events = eventService.findAll();
-        } catch (ResponseStatusException e) {
-            events = new ArrayList<>();
-        }
-        return new ServerRecommendationService(events);
-    }
 }
