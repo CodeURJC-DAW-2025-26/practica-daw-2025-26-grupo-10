02762625ce8e7a3bb06d23daf63ddd1b90e4769d
fuihfuefuiewn <danielbonachelamartinez@gmail.com>
2026-04-15 18:26:14 +0200

    Header react

---
+++ frontend/tickethub/app/components/adminNavBar/AdminNavMenu.tsx
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router";
+
+export const AdminNavMenu = () => {
+    return (
+        <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+                <NavLink className="nav-link" to="/admin/events/manage_events">
+                    Gestionar Eventos
+                </NavLink>
+            </li>
+            <li className="nav-item">
+                <NavLink className="nav-link" to="/admin/artists/manage_artists">
+                    Gestionar Artistas
+                </NavLink>
+            </li>
+            <li className="nav-item">
+                <NavLink className="nav-link" to="/admin/statistics">
+                    Estadísticas
+                </NavLink>
+            </li>
+        </ul>
+    );
+};
