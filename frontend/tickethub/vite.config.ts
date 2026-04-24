f9194ecf1c20dea299747e328cc18872ce64e798
rualpa2004 <146848892+rualpa2004@users.noreply.github.com>
2026-04-24 13:22:51 +0200

    dependencies hot fix

--- frontend/tickethub/vite.config.ts
+++ frontend/tickethub/vite.config.ts
@@ -1,16 +1,16 @@
 import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
  
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths()],
   server: {
     proxy: {
       "/api": {
         target: "https://localhost:8443",
         changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
       },
     },
   },
