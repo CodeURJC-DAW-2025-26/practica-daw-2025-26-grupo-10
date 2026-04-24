b2c6c8fb1ac7b7b83970aea55a6c2f99bb5404a7
shania <119068473+shaniaconx@users.noreply.github.com>
2026-04-24 21:54:19 +0200

    Styles and TicketHub icon

--- frontend/tickethub/app/layout/MainLayout.tsx
+++ frontend/tickethub/app/layout/MainLayout.tsx
@@ -2,6 +2,8 @@ import { Outlet, useLocation, useNavigation } from "react-router";
 import {AdminHeader} from "./adminHeader";
 import AdminFooter from "./adminFooter";
 import GlobalSpinner from "~/components/GlobalSpinner";
+import { PublicHeader } from "./publicHeader";
+import { PublicFooter } from "./publicFooter";
 
 const MainLayout = () => {
     const location = useLocation();
@@ -10,15 +12,17 @@ const MainLayout = () => {
     const isLoading = navigation.state === "loading";
     
     return (
-        <>
+        <div className="d-flex flex-column min-vh-100">
             {isAdmin ? <AdminHeader /> : <PublicHeader />}
+            
+            <main className="flex-fill">
+                {isLoading && <GlobalSpinner />}
 
-            {isLoading && <GlobalSpinner />}
-
-            <Outlet />
+                <Outlet />
+            </main>
 
             {isAdmin ? <AdminFooter /> : <PublicFooter />}
-        </>
+        </div>
     );
 };
 
